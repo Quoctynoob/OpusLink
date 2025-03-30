cd51cf5b08507e3f4c786ac7c76f5b855f428687
Quoctynoob <tilburgquoc34@gmail.com>
2025-03-29 23:26:08 -0400

    Organize svgs

--- app/components/JobListingCard.tsx
+++ app/components/JobListingCard.tsx
@@ -5,6 +5,7 @@ import { JobListing } from "../types";
 import { useAuth } from "../context/AuthContext";
 import { addAppliedJob, isJobApplied } from "../lib/appliedJobsService";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 interface JobListingCardProps {
   job: JobListing;
@@ -116,36 +117,24 @@ export default function JobListingCard({ job }: JobListingCardProps) {
           >
             {isJobInAppliedList ? (
               <>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 mr-1"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                <Image 
+                  src="/svg/check-icon.svg"
+                  width={20}
+                  height={20}
+                  alt="Checkmark"
+                  className="h-5 w-5 mr-1 text-current"
+                />
                 Added
               </>
             ) : (
               <>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 mr-1"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                  />
-                </svg>
+                <Image 
+                  src="/svg/plus-icon.svg"
+                  width={20}
+                  height={20}
+                  alt="Plus sign"
+                  className="h-5 w-5 mr-1 text-current"
+                />
                 {isAddingToList ? "Adding..." : "Track Application"}
               </>
             )}
